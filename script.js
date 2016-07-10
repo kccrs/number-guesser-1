b23424489a558f36c6e4becc3b0f18f8eb5aa6b9
Casey Cross <hellocaseycross@gmail.com>
2016-07-10 14:38:36 -0600

    JavaScript refactored

--- script.js
+++ script.js
@@ -7,31 +7,44 @@ var resetButton = document.querySelector('.reset');
 var pickANumber = document.querySelector('.pick-a-number');
 var rangeButton = document.querySelector('.range-button');
 var storedNumber;
+
 function randomNumberGenerator(min, max) {
-  var num = Math.floor(Math.random() * (max - min + 1)) + min;
-  storedNumber = num;
-  return num;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-//Range button stuff.
+function getMinNumber() {
+  return parseInt(document.querySelector('.min-range').value);
+}
+
+function getMaxNumber() {
+  parseInt(document.querySelector('.max-range').value);
+}
+
+function updateGuessLimits(min, max) {
+  guessInput.setAttribute('min', min);
+  guessInput.setAttribute('max', max);
+}
+
+
 
 rangeButton.addEventListener('click', function(){
-  var userMin = parseInt(document.querySelector('.min-range').value);
-  guessInput.setAttribute("min", userMin);
-  var userMax = parseInt(document.querySelector('.max-range').value);
-  guessInput.setAttribute("max", userMax);
+  var userMin = getMinNumber();
+  var userMax = getMaxNumber();
+
+  updateGuessLimits(userMin, userMax);
+
   var randomNumber = randomNumberGenerator(userMin, userMax);
+
   if (userMin !== '' || userMax !== '') {
     resetButton.disabled = false;
     guessButton.disabled = false;
   }
+
   if (userMin > userMax) {
     guessButton.disabled = true;
     pickANumber.innerText = 'That is not an acceptable range.';
   }
 });
-
-//guess button stuff
 
 guessButton.addEventListener('click', function () {
   var theGuess = parseInt(document.querySelector('.number-guess').value);
@@ -42,6 +55,7 @@ guessButton.addEventListener('click', function () {
 
   lastGuess.innerText = parseInt(guessInput.value, 10);
   pickANumber.innerText = "Your last guess was...";
+
   if (isNaN(storedNumber)) {
     rightOrWrong.innerText = "You haven't picked a range yet. Please pick a range and try again."
   }
@@ -50,16 +64,19 @@ guessButton.addEventListener('click', function () {
     lastGuess.innerText = "Not a number."
   }
 
-  else if (theGuess < userMin || theGuess > userMax) {
+  if (theGuess < userMin || theGuess > userMax) {
     rightOrWrong.innerText = "That number is outside of the acceptable range. Try again."
   }
-  else if (theGuess < storedNumber) {
+
+  if (theGuess < storedNumber) {
   rightOrWrong.innerText = 'Sorry that guess is too low. Pick a higher number.';
   }
-  else if (theGuess > storedNumber) {
+
+  if (theGuess > storedNumber) {
   rightOrWrong.innerText = 'Sorry that guess is too high. Pick a lower number.';
   }
-  else if (theGuess === storedNumber) {
+
+  if (theGuess === storedNumber) {
   guessInput.value = '';
   clearButton.disabled = true;
   rightOrWrong.innerText = 'Now try again, with more numbers to guess from.';
@@ -72,28 +89,20 @@ guessButton.addEventListener('click', function () {
   rangeButton.disabled = true;
   document.querySelector('.min-range').readOnly = true;
   document.querySelector('.max-range').readOnly = true;
-
-};
+  };
 });
 
-guessInput.addEventListener('change', function() {
+function changeButtons() {
   changeClearButton();
   changeResetButton();
-});
+}
 
-guessInput.addEventListener('keyup', function() {
-  changeClearButton();
-  changeResetButton();
-});
 
-//Clear button stuff
+guessInput.addEventListener('change', changeButtons);
+guessInput.addEventListener('keyup', changeButtons);
 
 function changeClearButton() {
-  if (guessInput.value === '') {
-  clearButton.disabled = true;
-} else {
-  clearButton.disabled = false;
-  }
+  clearButton.disabled = guessInput.value === '';
 };
 
 clearButton.addEventListener('click', function() {
@@ -101,15 +110,12 @@ clearButton.addEventListener('click', function() {
   clearButton.disabled = true;
 });
 
-//Reset button stuff
-
 function changeResetButton() {
   var minInput = document.querySelector('.min-range');
   var maxInput = document.querySelector('.max-range');
   if (guessInput.value !== '' || rightOrWrong.innerText !== '') {
     resetButton.disabled = false;
-  }
-  else {
+  } else {
     resetButton.disabled = true;
   }
 };
@@ -127,5 +133,4 @@ resetButton.addEventListener('click', function() {
   changeResetButton();
   clearButton.disabled = true;
   rangeButton.disabled = false;
-
 });
