d9550215706da54de569ad76fb57e4065977976a
Casey Cross <hellocaseycross@gmail.com>
2016-07-10 18:17:26 -0600

    Even more refactoring.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Number Guesser - July 2016 by Casey Cross and Brett Hevia
+
 var lastGuess = document.querySelector('.last-guess')
 var guessInput = document.querySelector('.number-guess');
 var guessButton = document.querySelector('.guess-button');
@@ -6,7 +8,13 @@ var clearButton = document.querySelector('.clear');
 var resetButton = document.querySelector('.reset');
 var pickANumber = document.querySelector('.pick-a-number');
 var rangeButton = document.querySelector('.range-button');
-var storedNumber;
+
+var userMin;
+var userMax;
+// var randomNumber;
+var theGuess;
+var minInputBox;
+var maxInputBox;
 
 function randomNumberGenerator(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -17,7 +25,7 @@ function getMinNumber() {
 }
 
 function getMaxNumber() {
-  parseInt(document.querySelector('.max-range').value);
+  return parseInt(document.querySelector('.max-range').value);
 }
 
 function updateGuessLimits(min, max) {
@@ -25,15 +33,12 @@ function updateGuessLimits(min, max) {
   guessInput.setAttribute('max', max);
 }
 
-
-
 rangeButton.addEventListener('click', function(){
-  var userMin = getMinNumber();
-  var userMax = getMaxNumber();
-
+  userMin = getMinNumber();
+  userMax = getMaxNumber();
   updateGuessLimits(userMin, userMax);
 
-  var randomNumber = randomNumberGenerator(userMin, userMax);
+  randomNumber = randomNumberGenerator(userMin, userMax);
 
   if (userMin !== '' || userMax !== '') {
     resetButton.disabled = false;
@@ -47,36 +52,26 @@ rangeButton.addEventListener('click', function(){
 });
 
 guessButton.addEventListener('click', function () {
-  var theGuess = parseInt(document.querySelector('.number-guess').value);
-  var userMin = parseInt(document.querySelector('.min-range').value);
-  var userMax = parseInt(document.querySelector('.max-range').value);
-  var minInputBox = document.querySelector('.min-range');
-  var maxInputBox = document.querySelector('.max-range');
+  theGuess = parseInt(guessInput.value);
 
-  lastGuess.innerText = parseInt(guessInput.value, 10);
+  userMin = getMinNumber();
+  userMax = getMaxNumber();
+
+  minInputBox = document.querySelector('.min-range');
+  maxInputBox = document.querySelector('.max-range');
+
+  lastGuess.innerText = theGuess;
   pickANumber.innerText = "Your last guess was...";
 
-  if (isNaN(storedNumber)) {
+  if (isNaN(userMin) || isNaN(userMax)) {
     rightOrWrong.innerText = "You haven't picked a range yet. Please pick a range and try again."
   }
   if (isNaN(theGuess)) {
     rightOrWrong.innerText = "That's not a number. Try again.";
-    lastGuess.innerText = "Not a number."
+    lastGuess.innerText = "Definitely not a number."
   }
 
-  if (theGuess < userMin || theGuess > userMax) {
-    rightOrWrong.innerText = "That number is outside of the acceptable range. Try again."
-  }
-
-  if (theGuess < storedNumber) {
-  rightOrWrong.innerText = 'Sorry that guess is too low. Pick a higher number.';
-  }
-
-  if (theGuess > storedNumber) {
-  rightOrWrong.innerText = 'Sorry that guess is too high. Pick a lower number.';
-  }
-
-  if (theGuess === storedNumber) {
+  if (theGuess === randomNumber) {
   guessInput.value = '';
   clearButton.disabled = true;
   rightOrWrong.innerText = 'Now try again, with more numbers to guess from.';
@@ -90,7 +85,20 @@ guessButton.addEventListener('click', function () {
   document.querySelector('.min-range').readOnly = true;
   document.querySelector('.max-range').readOnly = true;
   };
+
+  if (theGuess < randomNumber) {
+  rightOrWrong.innerText = 'Sorry that guess is too low. Pick a higher number.';
+  }
+
+  if (theGuess > randomNumber) {
+  rightOrWrong.innerText = 'Sorry that guess is too high. Pick a lower number.';
+  }
+
+  if (theGuess < userMin || theGuess > userMax) {
+    rightOrWrong.innerText = "That number is outside of the acceptable range. Try again."
+  }
 });
+
 
 function changeButtons() {
   changeClearButton();
@@ -129,7 +137,7 @@ resetButton.addEventListener('click', function() {
   guessInput.value = '';
   rightOrWrong.innerText = '';
   pickANumber.innerText = 'Pick a minimum and maximum number.'
-  randomNumber = Math.floor(Math.random() * 100);
+  randomNumberGenerator();
   changeResetButton();
   clearButton.disabled = true;
   rangeButton.disabled = false;
